fb2acd4c72b22a5d5a8df9279e3c2728e5c31ed7
Michael Virga <michael@studiomav.com>
2023-03-09 12:03:09 -0800

    port fix

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.use('/', routes);
 
 sequelize.sync({ force: false }).then(() =>
 {
-    app.listen(3001, () =>
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () =>
     {
-        console.log('App listening on port 3001');
+        console.log(`App listening on port ${PORT}`);
     });
 });
