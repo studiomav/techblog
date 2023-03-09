9e76548a392f812a651bf1fd970ea8378d207ed9
Michael Virga <michael@studiomav.com>
2023-03-09 12:08:10 -0800

    db port fix

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+const DB_PORT = process.env.DB_PORT || 3306,
 sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,
-    {
+    { 
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        port: 3306,
+        port: DB_PORT,
         define:
         {
             timestamps: 'false'
